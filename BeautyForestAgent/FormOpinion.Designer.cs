fec704a6aa9e3e381a4f45a67fe31cdc2e09ee59
HongEunBeen <vvvv980@gmail.com>
2019-09-17 11:46:52 +0900

    #2 폼 디자인 by 3617 홍은빈

--- BeautyForestAgent/FormOpinion.Designer.cs
+++ BeautyForestAgent/FormOpinion.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOpinion));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tlsMenu = new System.Windows.Forms.ToolStrip();
+            this.fontDlg = new System.Windows.Forms.FontDialog();
+            this.colorDlg = new System.Windows.Forms.ColorDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tsbtnColor = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnFont = new System.Windows.Forms.ToolStripButton();
+            this.tlsMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tlsMenu
+            // 
+            this.tlsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnColor,
+            this.tsbtnFont});
+            this.tlsMenu.Location = new System.Drawing.Point(0, 0);
+            this.tlsMenu.Name = "tlsMenu";
+            this.tlsMenu.Size = new System.Drawing.Size(800, 25);
+            this.tlsMenu.TabIndex = 1;
+            this.tlsMenu.Text = "toolStrip1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(63, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(648, 360);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // tsbtnColor
+            // 
+            this.tsbtnColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnColor.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnColor.Image")));
+            this.tsbtnColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnColor.Name = "tsbtnColor";
+            this.tsbtnColor.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnColor.Text = "toolStripButton1";
+            // 
+            // tsbtnFont
+            // 
+            this.tsbtnFont.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnFont.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnFont.Image")));
+            this.tsbtnFont.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnFont.Name = "tsbtnFont";
+            this.tsbtnFont.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnFont.Text = "toolStripButton2";
+            // 
+            // FormOpinion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tlsMenu);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "FormOpinion";
             this.Text = "FormOpinion";
+            this.tlsMenu.ResumeLayout(false);
+            this.tlsMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip tlsMenu;
+        private System.Windows.Forms.ToolStripButton tsbtnColor;
+        private System.Windows.Forms.ToolStripButton tsbtnFont;
+        private System.Windows.Forms.FontDialog fontDlg;
+        private System.Windows.Forms.ColorDialog colorDlg;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
