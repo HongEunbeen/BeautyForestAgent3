0ab3c2b11d2cc7f099b24671dc05e5b9089fc4dd
HongEunBeen <vvvv980@gmail.com>
2019-09-17 11:54:36 +0900

    #3 코드 구현 by 3617 홍은빈

--- BeautyForestAgent/FormOpinion.Designer.cs
+++ BeautyForestAgent/FormOpinion.Designer.cs
@@ -33,14 +33,19 @@
             this.tlsMenu = new System.Windows.Forms.ToolStrip();
             this.fontDlg = new System.Windows.Forms.FontDialog();
             this.colorDlg = new System.Windows.Forms.ColorDialog();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
             this.tsbtnColor = new System.Windows.Forms.ToolStripButton();
             this.tsbtnFont = new System.Windows.Forms.ToolStripButton();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
             this.tlsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 428);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(800, 22);
@@ -59,13 +64,13 @@
             this.tlsMenu.TabIndex = 1;
             this.tlsMenu.Text = "toolStrip1";
             // 
-            // richTextBox1
+            // rtbText
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(63, 29);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(648, 360);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.rtbText.Location = new System.Drawing.Point(63, 29);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(648, 360);
+            this.rtbText.TabIndex = 2;
+            this.rtbText.Text = resources.GetString("rtbText.Text");
             // 
             // tsbtnColor
             // 
@@ -75,6 +80,7 @@
             this.tsbtnColor.Name = "tsbtnColor";
             this.tsbtnColor.Size = new System.Drawing.Size(23, 22);
             this.tsbtnColor.Text = "toolStripButton1";
+            this.tsbtnColor.Click += new System.EventHandler(this.TsbtnColor_Click);
             // 
             // tsbtnFont
             // 
@@ -84,17 +90,37 @@
             this.tsbtnFont.Name = "tsbtnFont";
             this.tsbtnFont.Size = new System.Drawing.Size(23, 22);
             this.tsbtnFont.Text = "toolStripButton2";
+            this.tsbtnFont.Click += new System.EventHandler(this.TsbtnFont_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(144, 17);
+            this.toolStripStatusLabel1.Text = "3617 홍은빈  - 폰트 변경";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(710, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "전송";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // FormOpinion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbText);
             this.Controls.Add(this.tlsMenu);
             this.Controls.Add(this.statusStrip1);
             this.Name = "FormOpinion";
             this.Text = "FormOpinion";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.tlsMenu.ResumeLayout(false);
             this.tlsMenu.PerformLayout();
             this.ResumeLayout(false);
@@ -110,6 +136,8 @@
         private System.Windows.Forms.ToolStripButton tsbtnFont;
         private System.Windows.Forms.FontDialog fontDlg;
         private System.Windows.Forms.ColorDialog colorDlg;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
     }
 }
